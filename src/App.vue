10527c33dedb2af5045921a796296ff2877fba26
Exem <chernyakov.ghost@mail.ru>
2022-03-29 08:44:48 +0500

    фвцф

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+<Chart
+    :size="{ width: 500, height: 400 }"
+    :data="data"
+    :margin="margin"
+    :direction="direction">
+
+    <template #layers>
+      <Grid strokeDasharray="2,2" />
+      <Line :dataKeys="['name', 'pl']" />
+    </template>
+
+  </Chart>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, ref } from "vue";
+import { Chart, Grid, Line } from 'vue3-charts'
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+  name: "GraphsApp",
+  components: { Chart, Grid, Line },
+  setup() {
+    const plByMonth = [
+  { name: 'Jan', pl: 1000, avg: 500, inc: 300 },
+  { name: 'Feb', pl: 2000, avg: 900, inc: 400 },
+  { name: 'Apr', pl: 400, avg: 400, inc: 500 },
+  { name: 'Mar', pl: 3100, avg: 1300, inc: 700 },
+  { name: 'May', pl: 200, avg: 100, inc: 200 },
+  { name: 'Jun', pl: 600, avg: 400, inc: 300 },
+  { name: 'Jul', pl: 500, avg: 90, inc: 100 }
+]
+    const data = ref(plByMonth)
+    const direction = ref('horizontal')
+    const margin = ref({
+      left: 0,
+      top: 20,
+      right: 20,
+      bottom: 0
+    })
+
+    return { data, direction, margin }
+  }
 });
 </script>
 
